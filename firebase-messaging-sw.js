80c87ff654209b2caa448ad05ed03c7215cee950
gopespinnacle <gopestalks@gmail.com>
2026-09-18 13:32:29 +0530

    asdsad

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,14 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBpiA8270xJXQiLfreO-F4GqKdWhXXdkE",
-  authDomain: "gopes-pinnacle-academy.firebaseapp.com",
-  projectId: "gopes-pinnacle-academy",
-  storageBucket: "gopes-pinnacle-academy.firebasestorage.app",
-  messagingSenderId: "445901611941",
-  appId: "1:445901611941:web:67b591a372eb5c52fb3e4c",
-  measurementId: "G-M6GFBXPN4S"
+    apiKey: "AIzaSyCBpiA8270xJXQiLfreO-F4GqKdWhXXdkE",
+    authDomain: "gopes-pinnacle-academy.firebaseapp.com",
+    projectId: "gopes-pinnacle-academy",
+    storageBucket: "gopes-pinnacle-academy.firebasestorage.app",
+    messagingSenderId: "445901611941",
+    appId: "1:445901611941:web:67b591a372eb5c52fb3e4c"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 
